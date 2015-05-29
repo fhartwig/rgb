7ed84dd7e759ebefe987a4d49984eafce1551580
Florian Hartwig <florian.j.hartwig@gmail.com>
2015-05-29 19:47:16 +0200

    Fix checksum computation

--- src/rom.rs
+++ src/rom.rs
@@ -3,6 +3,7 @@ use std::result;
 pub struct Header {
     pub begin_code_execution_point: [u16; 2],
     pub game_title: [u8; 16],
+    cartridge_type: CartridgeType,
     rom_size: u8, // number of banks
     ram_size: u8, // number of banks
     destination_code: DestinationCode,
@@ -31,17 +32,20 @@ impl Header {
         //let header_bytes = rom[HEADER_OFFSET..HEADER_OFFSET + HEADER_LENGTH];
         let mut title = [0; 16];
         copy_memory(&rom[0x134..0x143], &mut title);
-        let begin_execution = [(rom[100] as u16) << 1 | rom[101] as u16,
-                               (rom[102] as u16) << 1 | rom[103] as u16];
+        let begin_execution = [(rom[100] as u16) << 8 | rom[101] as u16,
+                               (rom[102] as u16) << 8 | rom[103] as u16];
+        let cartridge_type = CartridgeType::from_u8(rom[0x147]);
+
         let header = Header {
             begin_code_execution_point: begin_execution,
             game_title: title,
+            cartridge_type: cartridge_type,
             rom_size: 0, // TODO
             ram_size: 0, // TODO
             destination_code: DestinationCode::from_u8(rom[0x14A]),
-            checksum: (rom[0x14E] as u16) << 1 | rom[0x14F] as u16
+            checksum: (rom[0x14E] as u16) << 8 | rom[0x14F] as u16
         };
-        //try!(header.check_checksum(rom));
+        try!(header.check_checksum(rom));
         Ok(header)
     }
 
@@ -49,10 +53,9 @@ impl Header {
         use std::num::wrapping::OverflowingOps;
 
         let mut sum = 0u16;
-        for byte in rom[..0x14E].iter().chain(rom[0x15..].iter()) {
+        for byte in rom[..0x14E].iter().chain(rom[0x150..].iter()) {
             sum = sum.overflowing_add(*byte as u16).0;
         }
-        println!("Sum: {}, expected: {}", sum, self.checksum);
         if sum == self.checksum { Ok(()) } else { Err(Error::BadChecksum) }
     }
 }
@@ -67,6 +70,35 @@ impl DestinationCode {
         match code {
             0 => DestinationCode::Japanese,
             _ => DestinationCode::NonJapanese
+        }
+    }
+}
+
+pub struct CartridgeType {
+    memory_bank: Option<MemoryBankController>
+}
+
+impl CartridgeType {
+    fn from_u8(n: u8) -> CartridgeType {
+        CartridgeType { memory_bank: MemoryBankController::from_u8(n) }
+    }
+}
+
+pub enum MemoryBankController {
+    Type1,
+    Type2,
+    Type3,
+    Type5,
+}
+
+impl MemoryBankController {
+    pub fn from_u8(n: u8) -> Option<MemoryBankController> {
+        match n {
+            1 | 2 | 3 => Some(MemoryBankController::Type1),
+            5 | 6 => Some(MemoryBankController::Type2),
+            0x12 | 0x13 => Some(MemoryBankController::Type3),
+            0x19...0x1E => Some(MemoryBankController::Type5),
+            _ => None
         }
     }
 }
